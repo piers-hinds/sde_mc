7ac50cd3c4ea4c686a986cc8a065f5b65956f4ba
Piers14 <61315972+Piers14@users.noreply.github.com>
2021-04-22 18:06:32 +0100

    monte carlo method and a monte class to store stats

--- sde_mc/driver.py
+++ sde_mc/driver.py
@@ -1,21 +1,16 @@
 from options import aon_payoff, aon_true
 from sde import Gbm, SdeSolver
+from mc import mc_simple
 import torch
+import numpy as np
 import time
-
 
 steps = 800
 trials = 10000
 
 gbm = Gbm(mu=0.02, sigma=0.2, init_value=torch.tensor([1.0]))
-test = SdeSolver(sde=gbm, time=3, num_steps=steps, dimension=1)
+solver = SdeSolver(sde=gbm, time=3, num_steps=steps, dimension=1)
 
+mc_stats = mc_simple(num_trials=trials, sde_solver=solver, payoff=aon_payoff, discount=np.exp(-0.06))
+mc_stats.print()
 
-start = time.time()
-out = test.euler(bs=trials)
-spots = out[:, steps, :].squeeze(-1)
-payoffs = aon_payoff(spots, 1) * torch.exp(torch.tensor(-0.06))
-end = time.time()
-print(end-start)
-print('True value: ', aon_true(1, 1, 0.02, 0.2, 3))
-print(torch.mean(payoffs), "+/-", 2 * torch.std(payoffs) / torch.sqrt(torch.tensor(trials)))
---
+++ sde_mc/mc.py
@@ -0,0 +1,51 @@
+import torch
+import time
+
+
+class MCStatistics:
+    """A class to store relevant Monte Carlo statistics"""
+
+    def __init__(self, sample_mean, sample_std, time, paths=None, payoffs=None):
+        """
+        :param sample_mean: torch.tensor, the mean of the samples
+        :param sample_std: torch.tensor, the standard deviation of the samples / sqrt(num_trials)
+        :param time: float, the time taken for the MC simulation
+        :param paths: torch.tensor (optional), the sample paths generated from the SDE
+        :param payoffs: torch.tensor (optional), all payoffs generated from the SDE
+        """
+        self.sample_mean = sample_mean.item()
+        self.sample_std = sample_std.item()
+        self.time = time
+        self.paths = paths
+        self.payoffs = payoffs
+
+    def print(self, num_std=2):
+        """Prints the mean, confidence interval, and time taken
+        :param num_std: float, the coefficient corresponding to fiducial probabilities - e.g. num_std = 2
+        corresponds to a 95% confidence interval
+        :return: None, prints output
+        """
+        print('Mean: {:.5f}  +/- {:.5f}     Time taken (s): {:.2f}'.format(self.sample_mean,
+                                                                           self.sample_std * num_std, self.time))
+
+
+def mc_simple(num_trials, sde_solver, payoff, discount=1):
+    """A simple Monte Carlo method applied to an SDE
+    :param num_trials: int, the number of MC simulations
+    :param sde_solver: SdeSolver, the solver for the SDE
+    :param payoff: function, a payoff function to be applied to the process at the final time step, it must be able to
+    be applied across a torch.tensor
+    :param discount: float (optional), a discount factor to be applied to the payoffs
+    :return: MCStatistics, the relevant statistics from the MC simulation - see MCStatistics class
+    """
+    start = time.time()
+    out = sde_solver.euler(bs=num_trials)
+    spots = out[:, sde_solver.num_steps, :].squeeze(-1)
+    payoffs = payoff(spots, 1) * discount
+    end = time.time()
+
+    mn = payoffs.mean()
+    sd = payoffs.std() / torch.sqrt(torch.tensor(num_trials))
+    tt = end - start
+
+    return MCStatistics(mn, sd, tt, out, payoffs)
