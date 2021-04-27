2da1e5e2da2b7b40f34ded38a84945b02b6c2dd8
Piers14 <61315972+Piers14@users.noreply.github.com>
2021-04-27 19:06:22 +0100

    control variate methods

--- sde_mc/driver.py
+++ sde_mc/driver.py
@@ -2,20 +2,35 @@ from options import aon_payoff, aon_true
 from sde import Gbm, SdeSolver
 from mc import mc_simple
 from regression import *
+from vreduction import GbmApproximator, SdeControlVariate
 import torch
 import numpy as np
 import time
 import matplotlib.pyplot as plt
 from functools import partial
 
-steps = 10
-trials = 1000
 
-gbm = Gbm(mu=0.02, sigma=0.2, init_value=torch.tensor([1.0, 1.0]))
-solver = SdeSolver(sde=gbm, time=3, num_steps=steps, dimension=2)
+def aon_payoff_cv(spot, strike):
+    return aon_payoff(spot[:, 0], strike) * np.exp(-0.06) + spot[:, 1]
+
+
+steps = 600
+trials = 10000
+
+gbm = Gbm(mu=0.02, sigma=0.2, init_value=torch.tensor([1.0]))
+solver = SdeSolver(sde=gbm, time=3, num_steps=steps, dimension=1)
 
 mc_stats = mc_simple(num_trials=trials, sde_solver=solver, payoff=aon_payoff, discount=np.exp(-0.06), bs=None, shared_noise=True)
 mc_stats.print()
+
+ts = torch.tensor([3*i / steps for i in range(1, steps+1)])
+gbm_approx = GbmApproximator(basis=[basis_1, basis_2, basis_3], ts=ts, mu=gbm.mu, sigma=gbm.sigma)
+gbm_approx.fit(paths=mc_stats.paths, payoffs=mc_stats.payoffs)
+
+gbm_cv = SdeControlVariate(base_sde=gbm, control_variate=gbm_approx, time_points=ts)
+cv_solver = SdeSolver(sde=gbm_cv, time=3, num_steps=steps, dimension=2)
+mc_stats_cv = mc_simple(num_trials=10000, sde_solver=cv_solver, payoff=aon_payoff_cv, shared_noise=True)
+mc_stats_cv.print()
 
 
 """
---
+++ sde_mc/vreduction.py
@@ -0,0 +1,108 @@
+from sde import Sde
+from regression import fit_basis
+import torch
+from abc import ABC, abstractmethod
+from functools import partial
+
+
+class ControlVariateApproximator(ABC):
+    """Abstract class for the approximate solutions to the SDEs"""
+
+    def __init__(self, basis, ts):
+        """
+        :param basis: list of functions, the basis functions to fit to the data
+        :param ts: torch.tensor, the time points at which the data is observed
+        """
+        self.basis = basis
+        self.ts = ts
+        self.coefs = None
+
+    def fit(self, paths, payoffs):
+        """Fits the basis functions to the data (paths, payoffs)
+        :param paths: torch.tensor, the paths as outputted from an SdeSolver
+        :param payoffs: torch.tensor, the payoffs as outputted from an SdeSolver
+        :return: None
+        """
+        self.coefs = torch.empty((paths.shape[1] - 1, len(self.basis)))
+        for i in range(1, paths.shape[1]):
+            current_basis = [partial(b, t=self.ts[i-1]) for b in self.basis]
+            self.coefs[i-1] = fit_basis(paths[:, i].squeeze(-1), payoffs, current_basis)
+
+    def derivative(self, time_idx, x):
+        """Computes the derivative at (time_idx, x) using automatic differentiation
+        :param time_idx: int, the index of the time points at which to evaluate the basis functions
+        :param x: torch.tensor, the value of the process
+        :return: torch.tensor, the approximate solution
+        """
+        basis_sum = 0
+        for i, b in enumerate(self.basis):
+            x.requires_grad = True
+            y = b(x, self.ts[time_idx])
+            y.backward(torch.ones_like(x))
+            x.requires_grad = False
+            grad = x.grad
+            x.grad = None
+            basis_sum += self.coefs[time_idx, i] * grad
+        return basis_sum
+
+    @abstractmethod
+    def __call__(self, time_idx, t, x):
+        """Override with your code here - depends on the Sde
+        :param time_idx: int, the index of the time points
+        :param t: torch.tensor
+        :param x: torch.tensor
+        :return: the approximation for F * Y (i.e. the diffusion term of the control variate process Z)
+        """
+        pass
+
+
+class GbmApproximator(ControlVariateApproximator):
+    """Approximator for GBM"""
+
+    def __init__(self, basis, ts, mu, sigma):
+        super(GbmApproximator, self).__init__(basis, ts)
+        self.mu = mu
+        self.sigma = sigma
+
+    def __call__(self, time_idx, t, x):
+        return torch.exp(-self.mu * t) * -self.sigma * self.derivative(time_idx, x)
+
+
+class SdeControlVariate(Sde):
+    """An Sde class which adds a control variate to an existing SDE"""
+
+    def __init__(self, base_sde, control_variate, time_points):
+        """
+        :param base_sde: Sde, the original SDE to add a control variate to
+        :param control_variate: ControlVariateApproximator, the approximation of F * Y
+        :param time_points: torch.tensor, the time points at which to update the control variate
+        """
+        super(SdeControlVariate, self).__init__(torch.cat([base_sde.init_value, torch.tensor([0.])]))
+        self.base_sde = base_sde
+        self.base_dim = len(base_sde.init_value)
+        self.cv = control_variate
+        self.time_points = time_points
+
+        self.idx = 0
+        self.F = 0
+
+    def drift(self, t, x):
+        return torch.cat([self.base_sde.drift(t, x[:, :self.base_dim]), torch.zeros_like(x[:, 1]).unsqueeze(1)], dim=-1)
+
+    def diffusion(self, t, x):
+        if not t:
+            self.reset_control(x)
+        if t >= self.time_points[self.idx]:
+            self.idx += 1
+            self.update_control(t, x)
+
+        return torch.cat([self.base_sde.diffusion(t, x[:, :self.base_dim]), self.F], dim=-1)
+
+    def update_control(self, t, x):
+        """Updates the control variate"""
+        self.F = self.cv(self.idx, t, x[:, :self.base_dim])
+
+    def reset_control(self, x):
+        """Resets the control variate (e.g. when restarting)"""
+        self.F = torch.zeros_like(x[:, :self.base_dim])
+        self.idx = 0
