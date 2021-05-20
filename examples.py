18a8630f4c3e7456f5ebdd64714867dec937b32c
Piers14 <61315972+Piers14@users.noreply.github.com>
2021-05-20 16:11:39 +0100

    basket options

--- examples.py
+++ examples.py
@@ -7,6 +7,29 @@ from functools import partial
 from abc import ABC, abstractmethod
 
 
+# Basket option on multi-dimensional GBM
+steps = 3000
+trials = 1000
+dim = 4
+
+x0 = torch.tensor(1.).repeat(dim)
+print(x0.unsqueeze(0))
+multi_gbm = Gbm(mu=0.02, sigma=torch.tensor([0.1, 0.2, 0.15, 0.3]), init_value=x0, dim=dim)
+solver = SdeSolver(sde=multi_gbm, time=3, num_steps=steps)
+mc_stats = mc_simple(num_trials=trials, sde_solver=solver, payoff=Basket(strike=1.), discount=np.exp(-0.06))
+mc_stats.print()
+
+
+steps = 300
+sf = int(3000 / steps)
+ts = torch.tensor([i * 3 / steps for i in range(1, steps+1)])
+multi_gbm = Gbm(mu=0.02, sigma=torch.tensor([0.1, 0.2, 0.15, 0.3]), init_value=x0, dim=dim)
+solver = SdeSolver(sde=multi_gbm, time=3, num_steps=steps)
+gbm_approx = NetApproximator(time_points=ts, layer_sizes=[15, 15], dim=dim, epochs=10, bs=1024)
+mc_stats_cv = mc_control_variate((300, 1000), simple_solver=solver, approximator=gbm_approx, payoff=Basket(strike=1.),
+                                 discounter=ConstantShortRate(r=0.02), step_factor=sf)
+mc_stats_cv.print()
+
 
 
 
--- sde_mc/options.py
+++ sde_mc/options.py
@@ -4,6 +4,23 @@ from scipy.integrate import quad
 from abc import ABC, abstractmethod
 
 
+def aon_true(spot, strike, r, vol, time):
+    """
+    Computes the true value of a binary asset-or-nothing option under GBM
+
+    :param spot: the spot price of the asset
+    :param strike: the strike price of the option
+    :param r: the risk-free rate
+    :param vol: the volatility of the asset
+    :param time: the time to maturity of the option
+    :return: the value of the option
+    """
+    upper_limit = ( np.log(spot / strike) + (r + 0.5 * vol * vol) * time ) / (np.sqrt(time) * vol)
+    lower_limit = -np.inf
+    value_integral = quad(lambda z: np.exp(- 0.5 * z * z), lower_limit, upper_limit)[0]
+    return value_integral / np.sqrt(2 * np.pi)
+
+
 class Option(ABC):
     """Abstract base class for options"""
     def __init__(self, log=False):
@@ -22,7 +39,7 @@ class EuroCall(Option):
         self.strike = strike
 
     def __call__(self, x):
-        spot = np.exp(x[:, 0]) if self.log else x[:, 0]
+        spot = torch.exp(x[:, 0]) if self.log else x[:, 0]
         return torch.where(spot > self.strike, spot - self.strike,
                            torch.tensor(0., dtype=spot.dtype, device=spot.device))
 
@@ -35,25 +52,22 @@ class BinaryAoN(Option):
         self.strike = strike
 
     def __call__(self, x):
-        spot = np.exp(x[:, 0]) if self.log else x[:, 0]
+        spot = torch.exp(x[:, 0]) if self.log else x[:, 0]
         return torch.where(spot >= self.strike, spot, torch.tensor(0, dtype=spot.dtype, device=spot.device))
 
 
-def aon_true(spot, strike, r, vol, time):
-    """
-    Computes the true value of a binary asset-or-nothing option under GBM
+class Basket(Option):
+    def __init__(self, strike, average_type='arithmetic', log=False):
+        assert average_type in ['arithmetic', 'geometric']
+        super(Basket, self).__init__(log)
+        self.strike = strike
+        self.average_type = average_type
 
-    :param spot: the spot price of the asset
-    :param strike: the strike price of the option
-    :param r: the risk-free rate
-    :param vol: the volatility of the asset
-    :param time: the time to maturity of the option
-    :return: the value of the option
-    """
-    upper_limit = ( np.log(spot / strike) + (r + 0.5 * vol * vol) * time ) / (np.sqrt(time) * vol)
-    lower_limit = -np.inf
-    value_integral = quad(lambda z: np.exp(- 0.5 * z * z), lower_limit, upper_limit)[0]
-    return value_integral / np.sqrt(2 * np.pi)
+    def __call__(self, x):
+        x = torch.exp(x) if self.log else x
+        spot = x.mean(1) if self.average_type == 'arithmetic' else torch.exp(torch.log(x).mean(1))
+        return torch.where(spot > self.strike, spot - self.strike, torch.tensor(0., dtype=spot.dtype,
+                                                                                device=spot.device))
 
 
 class ConstantShortRate:
