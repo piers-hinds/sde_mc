055170f713ee024a1f1c6b96aa51f452eff75f32
Piers <61315972+Piers14@users.noreply.github.com>
2021-08-03 14:02:46 +0100

    Update solvers.py

--- sde_mc/solvers.py
+++ sde_mc/solvers.py
@@ -111,6 +111,6 @@ class SdeSolver:
                 sol = solve_quadratic(coefs)
                 paths[:, i + 1, 1] = sol * sol
                 t += h
-        return paths, (_, corr_normals, _)
+        return paths, (None, corr_normals, None)
 
 
