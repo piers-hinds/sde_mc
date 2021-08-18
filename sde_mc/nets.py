740039802faac981c11dc83ec65a62443f220be1
Piers14 <61315972+Piers14@users.noreply.github.com>
2021-08-18 10:32:10 +0100

    Option for no batch norm on Mlp

--- sde_mc/nets.py
+++ sde_mc/nets.py
@@ -4,15 +4,20 @@ from torch.utils.data import Dataset
 
 
 class Mlp(nn.Module):
-    def __init__(self, input_size, layer_sizes, output_size, activation=nn.Tanh, final_activation=None):
+    def __init__(self, input_size, layer_sizes, output_size, activation=nn.Tanh, final_activation=None,
+                 batch_norm=True, batch_norm_init=True):
         assert len(layer_sizes) > 0, "At least one hidden layer required."
         super(Mlp, self).__init__()
         self.num_layers = len(layer_sizes)
+        layers = []
+        if batch_norm_init:
+            layers += [nn.BatchNorm1d(input_size)]
+        layers += [nn.Linear(input_size, layer_sizes[0]), nn.BatchNorm1d(layer_sizes[0]), activation()]
 
-        layers = [nn.BatchNorm1d(input_size), nn.Linear(input_size, layer_sizes[0]), nn.BatchNorm1d(layer_sizes[0]),
-                  activation()]
         for i in range(self.num_layers-1):
-            layers += [nn.Linear(layer_sizes[i], layer_sizes[i+1]), nn.BatchNorm1d(layer_sizes[i+1]), activation()]
+            layers += [nn.Linear(layer_sizes[i], layer_sizes[i+1]), activation()]
+            if batch_norm:
+                layers += [nn.BatchNorm1d(layer_sizes[i+1])]
         layers += [nn.Linear(layer_sizes[self.num_layers-1], output_size)]
         if final_activation is not None:
             layers += [final_activation()]
