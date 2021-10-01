9743a62f53a49cfb1bfaff3da18d86965e74a994
Piers <61315972+Piers14@users.noreply.github.com>
2021-10-01 11:48:19 +0100

    Changed structure of Sde class to allow for different diffusion matrix structures

--- sde_mc/sde.py
+++ sde_mc/sde.py
@@ -4,11 +4,64 @@ import numpy as np
 
 
 class Sde(ABC):
-    """Abstract base class for SDEs driven by a Wiener process and a Poisson process"""
+    """Abstract base class for SDEs driven by a Brownian motion and a Poisson process.
 
-    def __init__(self, init_value, dim, corr_matrix=None, method='euler'):
+    Each Sde object needs the following attributes:
+
+        init_value:
+            The initial value of the SDE
+
+        dim:
+            The dimension of the solution to the SDE
+
+        brown_dim:
+            The dimension of the Brownian motion driving the SDE
+
+        diffusion_struct:
+            The type of structure of the diffusion matrix of the SDE. In many cases the general setting of a
+            (dim, brown_dim)-shaped diffusion matrix is not efficient for simulation. Possible types include:
+
+                'diag': For when the diffusion matrix is diagonal (the brown_dim = dim). The output of Sde.diffusion()
+                    will be (dim,)-shaped
+
+                'indep': For when the diffusion matrix has two or more diagonal stripes corresponding to when the SDE is
+                    driven by two or more independent dim-dimensional brownian motions. The output of Sde.diffusion()
+                    will be (dim, brown_dim / dim)-shaped
+
+                'general': For the general case when there is no structure of the diffusion matrix. The shape of
+                    Sde.diffusion() will be (dim, brown_dim)
+
+        corr_matrix:
+            The correlation matrix for the Brownian motion. Defaults to the identity matrix.
+
+        method:
+            The preferred simulation method for the SDE. Defaults to Euler method and can be overridden when solving.
+    """
+
+    def __init__(self, init_value, dim, brown_dim, diffusion_struct, corr_matrix=None, method='euler'):
+        """
+        :param init_value: torch.tensor
+            The initial value of the SDE. Should be (dim,)-shaped
+
+        :param dim: int
+            The dimension of the solution to the SDE
+
+        :param brown_dim: int
+            The dimension of the Brownian motion driving the SDE
+
+        :param diffusion_struct: str
+            The type of structure of the diffusion matrix
+
+        :param corr_matrix: torch.tensor
+            The correlation matrix of the Brownian motion. Should be (brown_dim, brown_dim)-shaped
+
+        :param method: str
+            The preferred simulation method
+        """
         self.init_value = init_value
         self.dim = dim
+        self.diffusion_struct = diffusion_struct
+        self.brown_dim = brown_dim
         self.simulation_method = method
         if corr_matrix is not None:
             self.corr_matrix = corr_matrix
@@ -17,58 +70,91 @@ class Sde(ABC):
 
     @abstractmethod
     def drift(self, t, x):
-        """YOUR CODE HERE
-        :param t: torch.tensor, the current time
-        :param x: torch.tensor (bs, dim), the current value of the process
-        :return: torch.tensor (bs, dim), the drift vector of the process at (t, x)
+        """The drift of the SDE at time-space (t, x)
+
+        :param t: torch.tensor
+            The current time
+
+        :param x: torch.tensor (bs, dim)
+            The current value of the process
+
+        :return: torch.tensor (bs, dim)
+            The drift vector of the process at (t, x)
         """
         pass
 
     @abstractmethod
     def diffusion(self, t, x):
-        """YOUR CODE HERE
-        :param t: torch.tensor, the current time
-        :param x: torch.tensor (bs, dim), the current value of the process
-        :return: torch.tensor (bs, dim), the diffusion vector of the process at (t, x)
+        """The diffusion matrix of the SDE at time-space (t, x)
+
+        :param t: torch.tensor
+            The current time
+
+        :param x: torch.tensor (bs, dim)
+            The current value of the process
+
+        :return: torch.tensor
+            The diffusion matrix. It's shape depends on diffusion_struct:
+                diffusion_struct = 'diag' -> (bs, dim)
+                diffusion_struct = 'indep' -> (bs, dim, brown_dim / dim)
+                diffusion_struct = 'general' -> (bs, dim, brown_dim)
         """
         pass
 
     @abstractmethod
     def jumps(self, t, x, jumps):
-        """YOUR CODE HERE
-        :param t: torch.tensor, the current time
-        :param x: torch.tensor (bs, dim), the current value of the process
-        :param jumps: torch.tensor (bs, dim), the current jump sizes
-        :return: torch.tensor (bs, dim), the jump coefficient of the process at (t, x)
+        """The jump coefficient of the SDE at time-space (t, x)
+
+        :param t: torch.tensor
+            The current time
+
+        :param x: torch.tensor (bs, dim)
+            The current value of the process
+
+        :param jumps: torch.tensor (bs, dim)
+            The current jump sizes
+
+        :return: torch.tensor (bs, dim)
+            The jump coefficient of the process at (t, x)
         """
         pass
 
     @abstractmethod
     def sample_jumps(self, size, device):
-        """YOUR CODE HERE
-        :param size: tuple of ints, the size (shape) of the output of sampled jumps
-        :param device: str, the device on which to sample the jumps
-        :return: torch.tensor, sampled jumps
+        """Method to sample jumps
+        :param size: tuple of ints
+            The size (shape) of the output of sampled jumps
+
+        :param device: str
+            The device on which to sample the jumps
+
+        :return: torch.tensor
+            Sampled jumps
         """
         pass
 
     @abstractmethod
     def jump_mean(self):
-        """YOUR CODE HERE
-        :return: float, the mean of the jumps
+        """The expected value of the jump sizes
+
+        :return: torch.tensor
+            The mean of the jumps
         """
         pass
 
     @abstractmethod
     def jump_rate(self):
-        """YOUR CODE HERE
-        :return: float, the rate of the Poisson process
+        """The number of jumps per unit time
+
+        :return: torch.tensor
+            The rate of the Poisson process
         """
         pass
 
 
 class DiffusionSde(Sde):
     """Abstract class for SDEs with only a drift and diffusion component"""
+
     @abstractmethod
     def drift(self, t, x):
         pass
@@ -91,17 +177,26 @@ class DiffusionSde(Sde):
 
 
 class Gbm(DiffusionSde):
-    """Multi-dimensional GBM with possible correlation"""
+    """Multi-dimensional correlated geometric Brownian motion (GBM)"""
 
     def __init__(self, mu, sigma, init_value, dim, corr_matrix=None, method='euler'):
         """
-        :param mu: torch.tensor, the drifts of the process
-        :param sigma: torch.tensor, the volatilities of the process
-        :param init_value: torch.tensor, the initial value of the process
-        :param dim: torch.tensor, the dimension of the GBM
-        :param corr_matrix: torch.tensor, the correlation matrix
+        :param mu: torch.tensor
+            The drifts of the process
+
+        :param sigma: torch.tensor
+            The volatilities of the process
+
+        :param init_value: torch.tensor
+            The initial value of the process
+
+        :param dim: torch.tensor
+            The dimension of the GBM
+
+        :param corr_matrix: torch.tensor
+            The correlation matrix
         """
-        super(Gbm, self).__init__(init_value, dim, corr_matrix, method)
+        super(Gbm, self).__init__(init_value, dim, dim, 'diag', corr_matrix, method)
         self.mu = mu
         self.sigma = sigma
 
@@ -116,7 +211,7 @@ class Heston(DiffusionSde):
     def __init__(self, r, kappa, theta, xi, rho, init_value):
         assert 2 * kappa * theta > xi**2
         corr_matrix = torch.tensor([[1., rho], [rho, 1.]])
-        super(Heston, self).__init__(init_value, 2, corr_matrix)
+        super(Heston, self).__init__(init_value, 2, 2, 'diag', corr_matrix)
         self.simulation_method = 'heston'
         self.r = r
         self.kappa = kappa
@@ -139,16 +234,27 @@ class Heston(DiffusionSde):
 class LogNormalJumpsSde(Sde):
     """SDE with jumps that have shifted log-normal distribution"""
 
-    def __init__(self, rate, alpha, gamma, init_value, dim, corr_matrix=None, method='euler'):
+    def __init__(self, rate, alpha, gamma, init_value, dim, brown_dim, diffusion_struct, corr_matrix=None, method='euler'):
         """
-        :param rate: float, the rate of the Poisson process
-        :param alpha: float, the mean of the jumps
-        :param gamma: float, the standard deviation of the jumps
-        :param init_value: torch.tensor, the initial value of the process
-        :param dim: int, the dimension of the SDE
-        :param corr_matrix: torch.tensor, the correlation matrix
+        :param rate: float, torch.tensor
+            The rate of the Poisson process
+
+        :param alpha: float
+            The mean of the jumps
+
+        :param gamma: float
+            The standard deviation of the jumps
+
+        :param init_value: torch.tensor
+            The initial value of the process
+
+        :param dim: int
+            The dimension of the SDE
+
+        :param corr_matrix: torch.tensor
+            The correlation matrix
         """
-        super(LogNormalJumpsSde, self).__init__(init_value, dim, corr_matrix, method)
+        super(LogNormalJumpsSde, self).__init__(init_value, dim, brown_dim, diffusion_struct, corr_matrix, method)
         if torch.is_tensor(rate):
             self.rate = rate
         else:
@@ -183,18 +289,33 @@ class Merton(LogNormalJumpsSde):
 
     def __init__(self, mu, sigma, rate, alpha, gamma, init_value, dim, corr_matrix=None, method='euler'):
         """
-        :param mu: float, the drift of the process
-        :param sigma: float, the volatility of the process
-        :param rate: float, the rate of the Poisson process
-        :param alpha: float, the mean of the jumps
-        :param gamma: float, the standard deviation of the jumps
-        :param init_value: torch.tensor, the initial value of the process
-        :param dim: int, the dimension of the SDE
-        :param corr_matrix: torch.tensor, the correlation matrix
+        :param mu: float
+            The drift of the process
+
+        :param sigma: float
+            The volatility of the process
+
+        :param rate: float
+            The rate of the Poisson process
+
+        :param alpha: float
+            The mean of the jumps
+
+        :param gamma: float
+            The standard deviation of the jumps
+
+        :param init_value: torch.tensor
+            The initial value of the process
+
+        :param dim: int
+            The dimension of the SDE
+
+        :param corr_matrix: torch.tensor
+            The correlation matrix
         """
         self.mu = mu
         self.sigma = sigma
-        super(Merton, self).__init__(rate, alpha, gamma, init_value, dim, corr_matrix, method)
+        super(Merton, self).__init__(rate, alpha, gamma, init_value, dim, dim, 'diag', corr_matrix, method)
 
     def drift(self, t, x):
         return (self.mu - self.rate * self.jump_mean()) * x
